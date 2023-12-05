671e54dc13a76adb6ffb465238b3202a1f1d1c1b
Kendall Roberts <46752584+kendall1978@users.noreply.github.com>
2023-12-04 20:18:04 -0600

    CSS
    Updated some CSS animations and spacing on content

--- js/index.js
+++ js/index.js
@@ -9,3 +9,19 @@ burger.addEventListener('click', ()=> {
     myNav.classList.toggle("is-active")
     burger.classList.toggle("is-active")
 })
+
+const fadeInSection = document.querySelector(".fade-in-section")
+
+console.log(fadeInSection)
+
+window.addEventListener("scroll", (e) => {
+    let scroll = this.scrollY
+    console.log(scroll)
+    if (scroll >= 750) {
+        fadeInSection.classList.add("is-visible")
+    }
+    if (scroll < 250) {
+        fadeInSection.classList.remove("is-visible")
+    }
+})
+
